f69c4062f9635f79dd57d968137347084ba6ed17
evelinemou <evelinemou@yahoo.com>
2015-06-22 02:24:06 +0800

    commit

---
+++ cachematrix.R
@@ -0,0 +1,26 @@
+## Put comments here that give an over all description of what your 
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+
+        matrix(cache1 = cache2, cache2 = cache2,
+             cache3 = cache3,
+             cache4 = cache4)
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ##  Return a matrix that is the inverse of 'x'
+        cs <<-solve(x, ...)
+        cs
+}
